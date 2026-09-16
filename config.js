86b16d953ed970fafabe02e3810a349d21e1e183
jeiskwon <jeiskwon@gmail.com>
2026-09-16 15:14:21 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// 모찌 수호정령 공유 설정
+// Kakao Developers에서 새 앱을 만든 뒤 JavaScript 키만 아래에 넣으세요.
+// Admin 키 / REST API 키는 넣지 마세요.
+window.MOCHI_SHARE_CONFIG = {
+  kakaoJavaScriptKey: 'c9236350f9cdfcac7296e53f06a803ef',
+  publicBaseUrl: 'https://jeiskwon.github.io/mochi-saju/'
+};
